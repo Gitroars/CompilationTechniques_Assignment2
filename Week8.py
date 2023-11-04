93440a0a3e79b64ccf8bbff4aa9cca612c5d0550
Arvin <arvin.yuwono@binus.ac.id>
2023-11-04 18:26:32 +0700

    allow whitespace

--- Week8.py
+++ Week8.py
@@ -1,8 +1,8 @@
 import sys
-import math
 
 expression = ""
 index = 0
+
 
 class TreeNode:
     def __init__(self, value, left=None, right=None):
@@ -10,9 +10,11 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 def error(message):
     sys.stderr.write(f"Error: {message}\n")
     sys.exit(1)
+
 
 def match(expected_token):
     global index
@@ -24,48 +26,70 @@ def match(expected_token):
     else:
         error(f"Expected '{expected_token}'")
 
+
+def skip_whitespace():
+    global index
+    while index < len(expression) and expression[index].isspace():
+        index += 1
+
+
 def expr():
     temp = term()
 
     while index < len(expression) and (expression[index] in "+-"):
         op = expression[index]
+        skip_whitespace()
         match(op)
+        skip_whitespace()
         right = term()
         temp = TreeNode(op, temp, right)
 
     return temp
+
 
 def term():
     temp = factor()
 
     while index < len(expression) and (expression[index] in "*/%"):
         op = expression[index]
+        skip_whitespace()
         match(op)
+        skip_whitespace()
         right = factor()
         temp = TreeNode(op, temp, right)
 
     return temp
 
+
 def factor():
     global index
 
-    while index < len(expression) and expression[index].isspace():
-        index += 1
+    skip_whitespace()
 
-    if index < len(expression) and expression[index] == '(':
-        match('(')
+    if index < len(expression) and expression[index] == "(":
+        match("(")
         temp = expr()
-        match(')')
-    elif index < len(expression) and expression[index].isdigit():
-        value = 0
-        while index < len(expression) and expression[index].isdigit():
-            value = value * 10 + int(expression[index])
+        match(")")
+    elif index < len(expression) and (
+        expression[index].isdigit()
+        or (
+            expression[index] == "-"
+            and (index == 0 or (index > 0 and expression[index - 1] in "+-*/%"))
+        )
+    ):
+        value = ""
+        while index < len(expression) and (
+            expression[index].isdigit() or expression[index] == " "
+        ):
+            if expression[index] != " ":
+                value += expression[index]
             index += 1
-        temp = TreeNode(str(value))
+        temp = TreeNode(value)
     else:
         error("Invalid expression")
 
     return temp
+
 
 def display_parse_tree(node, level=0):
     if node is not None:
@@ -73,25 +97,27 @@ def display_parse_tree(node, level=0):
         print(" " * 4 * level + node.value)
         display_parse_tree(node.left, level + 1)
 
+
 def calculate(node):
-    if node.left is None and node.right is None:
-        return int(node.value)
-    left_result = calculate(node.left)
-    right_result = calculate(node.right)
-    if node.value == '+':
-        return left_result + right_result
-    elif node.value == '-':
-        return left_result - right_result
-    elif node.value == '*':
-        return left_result * right_result
-    elif node.value == '/':
-        if right_result == 0:
-            error("Division by zero")
-        return left_result / right_result
-    elif node.value == '%':
-        if right_result == 0:
-            error("Modulo by zero")
-        return left_result % right_result
+    if node.left is not None and node.right is not None:
+        left_result = calculate(node.left)
+        right_result = calculate(node.right)
+        if node.value == "+":
+            return left_result + right_result
+        elif node.value == "-":
+            return left_result - right_result
+        elif node.value == "*":
+            return left_result * right_result
+        elif node.value == "/":
+            if right_result == 0:
+                error("Division by zero")
+            return left_result / right_result
+        elif node.value == "%":
+            if right_result == 0:
+                error("Modulo by zero")
+            return left_result % right_result
+    return float(node.value)
+
 
 if __name__ == "__main__":
     expression = input("Enter the calculation string, e.g. '34 + 45 * 4 % 7': ")
@@ -99,10 +125,11 @@ if __name__ == "__main__":
 
     parse_tree = expr()
 
-    while index < len(expression) and expression[index].isspace():
-        index += 1
+    skip_whitespace()
 
-    if index == len(expression):
+    if index == len(expression) or (
+        index == len(expression) - 1 and expression[index] == " "
+    ):
         print("Parse Tree:")
         display_parse_tree(parse_tree)
         result = calculate(parse_tree)
