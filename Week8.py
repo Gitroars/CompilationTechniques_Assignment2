ad4cab366ff245d9e6219db0f40c166843d452c2
Arvin <arvin.yuwono@binus.ac.id>
2023-11-04 18:05:37 +0700

    allow whitespace

--- Week8.py
+++ Week8.py
@@ -1,8 +1,10 @@
 import sys
 import math
+import re
 
 expression = ""
-index = 0
+tokens = []
+
 
 class TreeNode:
     def __init__(self, value, left=None, right=None):
@@ -10,62 +12,59 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 def error(message):
     sys.stderr.write(f"Error: {message}\n")
     sys.exit(1)
 
-def match(expected_token):
-    global index
-    while index < len(expression) and expression[index].isspace():
-        index += 1
 
-    if index < len(expression) and expression[index] == expected_token:
-        index += 1
-    else:
-        error(f"Expected '{expected_token}'")
+def tokenize(expression):
+    # Define regular expressions for numbers, operators, and parentheses
+    regex = r"(\d+|\+|-|\*|/|%|\(|\))"
+    tokens = re.findall(regex, expression)
+    # Remove whitespace from the list of tokens
+    tokens = [token for token in tokens if not token.isspace()]
+    return tokens
+
 
 def expr():
     temp = term()
 
-    while index < len(expression) and (expression[index] in "+-"):
-        op = expression[index]
-        match(op)
+    while tokens and (tokens[0] in "+-"):
+        op = tokens.pop(0)
         right = term()
         temp = TreeNode(op, temp, right)
 
     return temp
 
+
 def term():
     temp = factor()
 
-    while index < len(expression) and (expression[index] in "*/%"):
-        op = expression[index]
-        match(op)
+    while tokens and (tokens[0] in "*/%"):
+        op = tokens.pop(0)
         right = factor()
         temp = TreeNode(op, temp, right)
 
     return temp
 
+
 def factor():
-    global index
+    token = tokens.pop(0)
 
-    while index < len(expression) and expression[index].isspace():
-        index += 1
-
-    if index < len(expression) and expression[index] == '(':
-        match('(')
+    if token == "(":
         temp = expr()
-        match(')')
-    elif index < len(expression) and expression[index].isdigit():
-        value = 0
-        while index < len(expression) and expression[index].isdigit():
-            value = value * 10 + int(expression[index])
-            index += 1
-        temp = TreeNode(str(value))
+        if tokens.pop(0) != ")":
+            error("Expected closing parenthesis")
+    elif token.isdigit() or (
+        token == "-" and (not tokens or tokens[0] in "0123456789")
+    ):
+        temp = TreeNode(token)
     else:
         error("Invalid expression")
 
     return temp
+
 
 def display_parse_tree(node, level=0):
     if node is not None:
@@ -73,36 +72,34 @@ def display_parse_tree(node, level=0):
         print(" " * 4 * level + node.value)
         display_parse_tree(node.left, level + 1)
 
+
 def calculate(node):
     if node.left is None and node.right is None:
         return int(node.value)
     left_result = calculate(node.left)
     right_result = calculate(node.right)
-    if node.value == '+':
+    if node.value == "+":
         return left_result + right_result
-    elif node.value == '-':
+    elif node.value == "-":
         return left_result - right_result
-    elif node.value == '*':
+    elif node.value == "*":
         return left_result * right_result
-    elif node.value == '/':
+    elif node.value == "/":
         if right_result == 0:
             error("Division by zero")
         return left_result / right_result
-    elif node.value == '%':
+    elif node.value == "%":
         if right_result == 0:
             error("Modulo by zero")
         return left_result % right_result
 
+
 if __name__ == "__main__":
     expression = input("Enter the calculation string, e.g. '34 + 45 * 4 % 7': ")
-    index = 0
-
+    tokens = tokenize(expression)
     parse_tree = expr()
 
-    while index < len(expression) and expression[index].isspace():
-        index += 1
-
-    if index == len(expression):
+    if not tokens:
         print("Parse Tree:")
         display_parse_tree(parse_tree)
         result = calculate(parse_tree)
